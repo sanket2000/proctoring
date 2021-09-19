31bf2f9826959589d270c0d3988e3aec8abcc618
Bhumit malvi <52121177+bhumitech-cloud@users.noreply.github.com>
2021-09-19 14:02:58 +0530

    Update screen.js
    added mouse script

--- screen.js
+++ screen.js
@@ -1,5 +1,5 @@
 console.log("Hello world");
-function myFunction() {
+function myFunction(event) {
 	var txt = "";
 	txt += "<p>innerWidth: " + window.innerWidth + "</p>";
 	txt += "<p>innerHeight: " + window.innerHeight + "</p>";
@@ -7,7 +7,8 @@ function myFunction() {
 	txt += "<p>outerHeight: " + window.outerHeight + "</p>";
 	txt += "<p>ScreenX: " + window.screenX + "</p>";
 	txt += "<p>ScreenY: " + window.screenY + "</p>";
-
+	txt += "<p>MouseX: " + event.offsetX + "</p>";
+	txt += "<p>MouseY: " + event.offsetY + "</p>";
 	document.getElementById("screenSize").innerHTML = txt;
 	console.log("Hello world");
 }
