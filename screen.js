62145ddef7a2b8886dcc1ea519ee33c9504d9985
Sanket <52121362+sanket2000@users.noreply.github.com>
2021-09-19 14:12:08 +0530

    Update screen.js

--- screen.js
+++ screen.js
@@ -3,7 +3,7 @@ console.log("Hello world");
 window.addEventListener("resize", myFunction);
 window.addEventListener("mousemove", myFunction);
 
-function myFunction() {
+function myFunction(event) {
 	var txt = "";
 	txt += "<p>innerWidth: " + window.innerWidth + "</p>";
 	txt += "<p>innerHeight: " + window.innerHeight + "</p>";
@@ -11,6 +11,8 @@ function myFunction() {
 	txt += "<p>outerHeight: " + window.outerHeight + "</p>";
 	txt += "<p>ScreenX: " + window.screenX + "</p>";
 	txt += "<p>ScreenY: " + window.screenY + "</p>";
+	txt += "<p>MouseX: " + event.offsetX + "</p>";
+	txt += "<p>MouseY: " + event.offsetY + "</p>";
 
 	document.getElementById("screenSize").innerHTML = txt;
 	console.log("Hello world");
