e2e521f273218103a683c21d944ab6eae5a665b7
Sanket <52121362+sanket2000@users.noreply.github.com>
2021-09-19 14:07:58 +0530

    added event listener

--- screen.js
+++ screen.js
@@ -1,5 +1,9 @@
 console.log("Hello world");
-function myFunction(event) {
+
+window.addEventListener("resize", myFunction);
+window.addEventListener("mousemove", myFunction);
+
+function myFunction() {
 	var txt = "";
 	txt += "<p>innerWidth: " + window.innerWidth + "</p>";
 	txt += "<p>innerHeight: " + window.innerHeight + "</p>";
@@ -7,8 +11,7 @@ function myFunction(event) {
 	txt += "<p>outerHeight: " + window.outerHeight + "</p>";
 	txt += "<p>ScreenX: " + window.screenX + "</p>";
 	txt += "<p>ScreenY: " + window.screenY + "</p>";
-	txt += "<p>MouseX: " + event.offsetX + "</p>";
-	txt += "<p>MouseY: " + event.offsetY + "</p>";
+
 	document.getElementById("screenSize").innerHTML = txt;
 	console.log("Hello world");
 }
